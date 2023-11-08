257c264e1ec19354a70652dfd59ac7cbd80ef7da
Nkechi Anyanwu <kechie,94@gmail.com>
2023-11-08 17:24:54 +0000

    Now using Sequelize to manage models and to seed a test database - Hooraygit add .

---
+++ database/models/checklist.js
@@ -0,0 +1,37 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Checklist extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Checklist.init({
+    item_name: DataTypes.STRING,
+    quantity: DataTypes.INTEGER,
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      onDelete: "CASCADE",
+      references: {
+        model: "Categories",
+        key: "id"
+      }
+    },
+    purchased: DataTypes.BOOLEAN
+  }, {
+    sequelize,
+    modelName: 'Checklist',
+    createdAt: "date_created",
+    updatedAt: "date_updated",
+    underscored: true,
+  });
+  return Checklist;
+};
---
+++ database/seeders/test/20231108170453-test-categories.js
@@ -0,0 +1,49 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    return queryInterface.sequelize.transaction(async (t) => {
+      await queryInterface.bulkInsert(
+        "Categories",
+        [
+          {
+            category_name: "Butcher",
+            date_created: "2023-11-08T14:14:01.390Z",
+            date_updated: "2023-11-08T14:14:01.390Z",
+          },
+          {
+            category_name: "Condiments",
+            date_created: "2023-11-08T14:14:01.390Z",
+            date_updated: "2023-11-08T14:14:01.390Z",
+          },
+          {
+            category_name: "Cleaning",
+            date_created: "2023-11-08T14:14:01.390Z",
+            date_updated: "2023-11-08T14:14:01.390Z",
+          }
+        ]);
+      })
+    },
+  
+    async down (queryInterface, Sequelize) {
+      /**
+       * Add commands to revert seed here.
+       *
+       * Example:
+       * await queryInterface.bulkDelete('People', null, {});
+       */
+      return queryInterface.sequelize.transaction(async (t) => {
+        await queryInterface.bulkDelete("Categories", null, {})
+      })
+    }
+  };
