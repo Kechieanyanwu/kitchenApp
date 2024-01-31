49ccc543649719c918d80e819a536cd3b1d6893b
Nkechi Anyanwu <kechie.94@gmail.com>
2024-01-31 19:36:40 +0000

    Included dummy login and register pages for app

--- src/server.js
+++ src/server.js
@@ -32,8 +32,71 @@ app.use("/checklist", checklistRouter);
 app.use("/inventory", inventoryRouter);
 
 app.get("/", (req, res) => {
-    res.status(200).json("Hello World")
+    // res.status(200).json("Hello World");
+    res.send('<h1>Home</h1><p>Please <a href="/register">register</a></p>');
 });
+
+// When you visit http://localhost:PORT/login, you will see "Login Page"
+app.get('/login', (req, res, next) => {
+   
+    const form = '<h1>Login Page</h1><form method="POST" action="/login">\
+    Enter Username:<br><input type="text" name="username">\
+    <br>Enter Password:<br><input type="password" name="password">\
+    <br><br><input type="submit" value="Submit"></form>';
+
+    res.send(form);
+
+});
+
+// When you visit http://localhost:PORT/register, you will see "Register Page"
+app.get('/register', (req, res, next) => {
+
+    const form = '<h1>Register Page</h1><form method="post" action="register">\
+                    Enter Username:<br><input type="text" name="username">\
+                    <br>Enter Password:<br><input type="password" name="password">\
+                    <br><br><input type="submit" value="Submit"></form>';
+
+    res.send(form);
+    
+});
+// todo
+app.post("/login", (req, res, next) => {})
+
+// todo
+app.post("/register", (req, res, next) => {})
+
+/**
+ * Lookup how to authenticate users on routes with Local Strategy
+ * Google Search: "How to use Express Passport Local Strategy"
+ * 
+ * Also, look up what behaviour express session has without a maxage set
+ */
+app.get('/protected-route', (req, res, next) => {
+    
+    // This is how you check if a user is authenticated and protect a route.  You could turn this into a custom middleware to make it less redundant
+    if (req.isAuthenticated()) {
+        res.send('<h1>You are authenticated</h1><p><a href="/logout">Logout and reload</a></p>');
+    } else {
+        res.send('<h1>You are not authenticated</h1><p><a href="/login">Login</a></p>');
+    }
+});
+
+// Visiting this route logs the user out
+app.get('/logout', (req, res, next) => {
+    req.logout();
+    res.redirect('/protected-route');
+});
+
+app.get('/login-success', (req, res, next) => {
+    res.send('<p>You successfully logged in. --> <a href="/protected-route">Go to protected route</a></p>');
+});
+
+app.get('/login-failure', (req, res, next) => {
+    res.send('You entered the wrong password.');
+});
+
+
+
 
 const server = app.listen(PORT, () => { 
     console.log(`Kitchen App is listening on port ${PORT}`)
