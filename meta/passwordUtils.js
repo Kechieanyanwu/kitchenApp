c7170534a782f0996cd5fbd2071d4c0e2143892e
Nkechi Anyanwu <kechie.94@gmail.com>
2024-02-01 14:27:30 +0000

    created Users table in db

--- meta/passwordUtils.js
+++ meta/passwordUtils.js
@@ -1,9 +1,19 @@
-const crypto = require('crypto');
-// to use bcrypt possibly
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 // TODO
-function comparePassword(password, hashedPassword, salt) {}
-function hashPassword(password) {}
+function comparePassword(plaintextPassword, hashedPassword) {
+    bcrypt.compare(plaintextPassword, hashedPassword, function (err, result) {
+        //check if result is true or false, do things based on it
+    })
+}
 
-module.exports.validPassword = validPassword; //rename
-module.exports.genPassword = genPassword;
+
+function hashPassword(plaintextPassword) {
+    bcrypt.hash(plaintextPassword, saltRounds, function (err, hash) {
+        //return hash? Or store hash in db?
+    })
+}
+
+module.exports.comparePassword = comparePassword; //rename
+module.exports.hashPassword = hashPassword;
