1a649e39551a283e0384784753859088fa4b40c8
Nkechi Anyanwu <kechie.94@gmail.com>
2024-02-06 15:41:07 +0000

    removed verbose comments

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -7,37 +7,20 @@ const { Inventory } = require("../../database/models/inventory");
 const nonExistentItemError = new Error("Nonexistent item")
 
 
-//REMEMBER TO PASS TRANSACTION T INTO EVERY SEQUELIZE CALL
-//WILL UPDATE THIS TO USE CLS IF VIBE DEY
-//will have to read up some more on transactions to see if i need to pass
-
-//do I want there to be a separate checkItem endpoint? Or do I just want it as an update? 
-
-
 // Beginning of functions
 const getAllItems = async (modelName, t ) => {
-    //I WANT TO ADD A CHECK FOR WHETHER THERE IS A T BEING PASSED, IF NOT, I CREATE A NEW TRANSACTION
-    // I think I'll add this when I integrate to the routers
-    // if (t === null) {
-    //     t = await sequelize.transaction();
-    // }
-    // t===null? t = await sequelize.transaction(): t; //test
-
-        try {
-                const items = await modelName.findAll(
-                // { raw: true, transaction: t }); 
-                { raw: true, attributes: {exclude: ["date_created", "date_updated"]}, transaction: t }); 
-                // { transaction: t }); 
-                return items;
-        } catch (error) {
-            throw error;
-    }
+    try {
+            const items = await modelName.findAll(
+            // { raw: true, transaction: t }); 
+            { raw: true, attributes: {exclude: ["date_created", "date_updated"]}, transaction: t }); 
+            // { transaction: t }); 
+            return items;
+    } catch (error) {
+        throw error;
+}
 }
 
 const getItem = async (modelName, itemID, t) => {
-
-    // t===null? t = await sequelize.transaction(): t; //test
-
     try{
             const requestedItem = await modelName.findByPk(itemID, 
                 { attributes: {exclude: ["date_created", "date_updated"]},
@@ -54,17 +37,14 @@ const getItem = async (modelName, itemID, t) => {
 
 
 const addNewItem = async(modelName, newItem, t) => {
-
-    // t===null? t = await sequelize.transaction(): t; //test
-    
     try {
-        const addedItem = await modelName.create(newItem, 
-            { transaction: t }); //seems you can't exclude columns on create, only on return, so will have to delete date columns
-        
+        const addedItem = await modelName.create(newItem,
+            { transaction: t });
+
         // remove these columns from result
         delete addedItem.dataValues.date_created;
         delete addedItem.dataValues.date_updated;
-        
+
         // return new item
         return addedItem.dataValues
 
@@ -76,8 +56,6 @@ const addNewItem = async(modelName, newItem, t) => {
 }
 
 const updateItem = async(modelName, itemID, desiredUpdate, t) => { 
-
-    // t===null? t = await sequelize.transaction(): t; //test
     let item;
     try {
         item = await validateID(itemID, modelName, t)
@@ -97,8 +75,6 @@ const updateItem = async(modelName, itemID, desiredUpdate, t) => {
 }
 
 const deleteItem = async (modelName, itemID, t) => {
-    // t===null? t = await sequelize.transaction(): t; //test
-
     let item;
     try {
         item = await validateID(itemID, modelName, t)
@@ -118,8 +94,6 @@ const deleteItem = async (modelName, itemID, t) => {
 }
 
 const moveCheckedItem = async (itemID, t) => {
-    //will I need to create a transaction here since multiple things are happening? 
-    
     let item;
     try {
         item = await validateID(itemID, Checklist, t)
@@ -142,7 +116,7 @@ const moveCheckedItem = async (itemID, t) => {
         { attributes: { exclude: ["date_created", "date_updated"] }, 
         transaction: t });
 
-    return updatedChecklist; //returns an updated checklist to be refreshed on the page
+    return updatedChecklist;
 }
 
 const validateNewGroceryItem = (req, res, next) => {
@@ -175,16 +149,16 @@ const validateNewGroceryItem = (req, res, next) => {
 };
 
 const validateNewCategory = (req, res, next) => {
-    if (JSON.stringify(req.body) == "{}") { //if an empty request body
+    if (JSON.stringify(req.body) == "{}") {
         const err = new Error("Empty Body");
         err.status = 400;
         next(err);
     } else {
         numKeysInReq = Object.keys(req.body);
-        if (req.body.category_name && (numKeysInReq.length == 1)) { //if there is just one key called category_name
-            if (typeof req.body.category_name === "string") { //if the category name is a string
+        if (req.body.category_name && (numKeysInReq.length == 1)) {
+            if (typeof req.body.category_name === "string") {
                 req.category_name = req.body.category_name;
-                next(); //go on to the next part of the middleware
+                next();
             } else {
                 const err = new Error("Category name must be a string");
                 err.status = 400;
@@ -202,8 +176,8 @@ const validateNewCategory = (req, res, next) => {
 
 const validateID = async (itemID, modelName, t) => {
     const item = await modelName.findByPk(itemID, 
-        { transaction: t }) //do I need plain: true everywhere? 
-    //check that the itemID exists
+        { transaction: t }) 
+
     if (item === null) {
         throw nonExistentItemError;
     }
